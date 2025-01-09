c636b289660da7fa283d50e117831bfa625e94df
kaspher01 <91979437+kaspher01@users.noreply.github.com>
2025-01-10 00:34:55 +0100

    updated rest config

--- src/rest/config/swagger.js
+++ src/rest/config/swagger.js
@@ -1,30 +1,28 @@
-import swaggerJsdoc from 'swagger-jsdoc';
-import swaggerUi from 'swagger-ui-express';
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
 
 const swaggerDefinition = {
-  openapi: '3.0.0',
+  openapi: "3.0.0",
   info: {
-    title: 'World Cup API',
-    version: '1.0.0',
-    description: 'API for managing World Cup teams and players',
+    title: "World Cup API",
+    version: "1.0.0",
+    description: "API for managing World Cup teams and players",
   },
-  servers: [
-    {
-      url: 'http://localhost:3000',
-      description: 'Local server',
-    },
-  ],
 };
 
 const options = {
   swaggerDefinition,
-  apis: ['./src/rest/routes/worldCups/*.js', './src/rest/routes/teams/*.js', './src/rest/routes/players/*.js'], 
+  apis: [
+    "./src/rest/routes/worldCups/*.js",
+    "./src/rest/routes/teams/*.js",
+    "./src/rest/routes/players/*.js",
+  ],
 };
 
 const swaggerSpec = swaggerJsdoc(options);
 
 const setupSwagger = (app) => {
-  app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 };
 
 export default setupSwagger;
